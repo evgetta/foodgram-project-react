9fb1c49066030379047c14301fbc9c41e7ad29ef
Evgeny Zaozerskikh <evgetta@gmail.com>
2023-04-14 07:22:33 +1100

    review_fix

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -5,8 +5,8 @@ from django.core.exceptions import ValidationError
 
 def is_hex_validator(color):
     color = color.strip(' #')
-    if not set(color).issubset(hexdigits) and len(color) not in (3, 6):
-        raise ValidationError(
-            'Неправильный hex-код цвета'
-        )
+    if not set(color).issubset(hexdigits):
+        raise ValidationError(f'Код цвета не является шестнадцатеричным')
+    if len(color) not in (3, 6):
+        raise ValidationError(f'Код цвета имеет неправильную длину')
     return '#' + color
